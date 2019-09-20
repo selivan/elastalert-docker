34c66c89b9e16432af4ed5c45d71352c54bc381e
Pavel Selivanov <selivan.at.github@gmail.com>
2019-09-20 17:39:59 +0300

    fix: remove extra argument in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 case "$1" in
     elastalert|elastalert-create-index|elastalert-test-rule)
+        program="$1"
         shift
-        exec "$1" --config /opt/config/config.yaml "$@"
+        exec "$program" --config /opt/config/config.yaml "$@"
         ;;
     *)
         echo "Usage: elastalert|elastalert-create-index|elastalert-test-rule [options]"
